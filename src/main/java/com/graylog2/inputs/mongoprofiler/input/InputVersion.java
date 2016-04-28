ea4d79b27fe2c9264d882a50533c8aec0356d5f0
Edmundo Alvarez <edmundo@graylog.com>
2016-04-28 10:19:41 +0200

    Bump to graylog 2.0

--- src/main/java/com/graylog2/inputs/mongoprofiler/input/InputVersion.java
+++ src/main/java/com/graylog2/inputs/mongoprofiler/input/InputVersion.java
@@ -7,7 +7,7 @@ import org.graylog2.plugin.Version;
  */
 public class InputVersion {
 
-    public static final Version PLUGIN_VERSION = new Version(1, 0, 1);
-    public static final Version REQUIRED_VERSION = new Version(1, 0, 0);
+    public static final Version PLUGIN_VERSION = new Version(1, 1, 0, "SNAPSHOT");
+    public static final Version REQUIRED_VERSION = new Version(2, 0, 0);
 
 }
